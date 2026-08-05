7082e28597a3726394dcc054f07dd8a2f2fbf499
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-08-05 16:26:42 -0700

    Load mobile search-to-map refinement

--- js/icon-upgrade.mjs
+++ js/icon-upgrade.mjs
@@ -2,6 +2,7 @@ import './map-mobile-refinement.mjs';
 import './map-profile-first-pass.mjs';
 import './mobile-tab-location-refinement.mjs';
 import './map-profile-aesthetic-refinement.mjs';
+import './search-map-refinement.mjs';
 import { createIcon, setIcon } from './icons.mjs';
 
 function replaceTextWithIcon(element, iconName, className = 'ui-icon') {
