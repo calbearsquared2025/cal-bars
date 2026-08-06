9e4203e5e162e761dad532b988536324271d0504
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-08-06 11:42:09 -0700

    style: load approved visual foundation fonts

---
+++ js/visual-foundations.mjs
@@ -0,0 +1,29 @@
+const FONT_STYLESHEET_ID = 'cgb-approved-fonts';
+const FOUNDATION_STYLESHEET_ID = 'cgb-visual-foundations';
+
+function appendStylesheet({ id, href, crossOrigin = '' }) {
+  if (document.getElementById(id)) return;
+  const link = document.createElement('link');
+  link.id = id;
+  link.rel = 'stylesheet';
+  link.href = href;
+  if (crossOrigin) link.crossOrigin = crossOrigin;
+  document.head.append(link);
+}
+
+function appendPreconnect(href, crossOrigin = '') {
+  if (document.head.querySelector(`link[rel="preconnect"][href="${href}"]`)) return;
+  const link = document.createElement('link');
+  link.rel = 'preconnect';
+  link.href = href;
+  if (crossOrigin) link.crossOrigin = crossOrigin;
+  document.head.append(link);
+}
+
+appendPreconnect('https://fonts.googleapis.com');
+appendPreconnect('https://fonts.gstatic.com', 'anonymous');
+appendStylesheet({
+  id: FONT_STYLESHEET_ID,
+  href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@600;700&family=Inter:wght@400;500;600;700&family=Source+Serif+4:opsz,wght@8..60,400;8..60,600;8..60,700&display=swap'
+});
+appendStylesheet({ id: FOUNDATION_STYLESHEET_ID, href: 'css/visual-foundations.css' });
