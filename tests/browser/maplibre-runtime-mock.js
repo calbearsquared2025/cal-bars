54e57acdf83e2e682aea19ee25415830d2369366
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-08-11 09:34:50 -0700

    Expose MapLibre runtime options for Detail tests

--- tests/browser/maplibre-runtime-mock.js
+++ tests/browser/maplibre-runtime-mock.js
@@ -1,4 +1,7 @@
 (() => {
+  const maps = [];
+  const markers = [];
+
   class LngLatBoundsMock {
     constructor() {
       this.points = [];
@@ -15,10 +18,17 @@
   }
 
   class MapMock {
-    constructor() {
+    constructor(options = {}) {
+      this.options = options;
       this.handlers = new globalThis.Map();
-      this.center = { lng: -98.5795, lat: 39.8283 };
-      this.zoom = 3.2;
+      const center = options.center || [-98.5795, 39.8283];
+      this.center = {
+        lng: Number(center?.lng ?? center?.[0] ?? -98.5795),
+        lat: Number(center?.lat ?? center?.[1] ?? 39.8283)
+      };
+      this.zoom = Number.isFinite(options.zoom) ? options.zoom : 3.2;
+      this.controls = [];
+      maps.push(this);
       queueMicrotask(() => this.emit('load'));
     }
 
@@ -41,7 +51,8 @@
       return this;
     }
 
-    addControl() {
+    addControl(control) {
+      this.controls.push(control);
       return this;
     }
 
@@ -90,17 +101,27 @@
   }
 
   class MarkerMock {
-    setLngLat() {
+    constructor(options = {}) {
+      this.options = options;
+      this.lngLat = null;
+      this.map = null;
+      markers.push(this);
+    }
+
+    setLngLat(value) {
+      this.lngLat = value;
       return this;
     }
 
-    addTo() {
+    addTo(map) {
+      this.map = map;
       return this;
     }
 
     remove() {}
   }
 
+  window.CGBMapLibreRuntimeMock = Object.freeze({ maps, markers });
   window.maplibregl = {
     Map: MapMock,
     Marker: MarkerMock,
