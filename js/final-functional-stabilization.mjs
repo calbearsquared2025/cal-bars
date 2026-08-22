164a40538d6736fa74ff7992455850c6e9228bee
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-08-22 10:31:17 -0700

    Remove obsolete clear-search selector

--- js/final-functional-stabilization.mjs
+++ js/final-functional-stabilization.mjs
@@ -240,7 +240,7 @@ function schedulePostRender() {
 }
 
 function setListLockFromEvent(event) {
-  if (event.target.closest?.('#mobile-list-button, #list-location-toggle, #clear-search-button')) {
+  if (event.target.closest?.('#mobile-list-button, #list-location-toggle')) {
     listSurfaceLocked = true;
     return;
   }
