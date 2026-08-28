f3ad01834cafd12cbf97ea391aa0144de9e6c0d0
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-08-28 13:31:10 -0700

    Load mobile direct venue profile bridge

--- js/icon-upgrade.mjs
+++ js/icon-upgrade.mjs
@@ -5,6 +5,7 @@ import './mobile-tab-location-refinement.mjs';
 import './map-profile-aesthetic-refinement.mjs';
 import './search-map-refinement.mjs';
 import './map-profile-final-pass.mjs';
+import './mobile-direct-venue-profile.mjs';
 import { renderMobileSelectedProfileContinuation } from './mobile-selected-profile-continuation.mjs';
 import { markerKind } from './core.mjs';
 import { createIcon, inlineSpriteIcons } from './icons.mjs';
