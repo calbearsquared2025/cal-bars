de60e745025a96590288716a037328e28b777775
Matthew Putzulu <calbearsquared2025@gmail.com>
2026-09-03 10:27:44 -0700

    Update browser renderer mock for MapTiler SDK

--- tests/browser/maplibre-runtime-mock.js
+++ tests/browser/maplibre-runtime-mock.js
@@ -128,7 +128,8 @@
   }
 
   window.CGBMapLibreRuntimeMock = Object.freeze({ maps, markers });
-  window.maplibregl = {
+  window.maptilersdk = {
+    config: { apiKey: '', session: true },
     Map: MapMock,
     Marker: MarkerMock,
     LngLatBounds: LngLatBoundsMock,
